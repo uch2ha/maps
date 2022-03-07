efa17afcd13d075408ea67fe5816883c9f4fbfcb
uch2ha <sinyavskiy.d@gmail.com>
2022-03-07 16:26:53 +0200

    done

--- App.js
+++ App.js
@@ -1,20 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+   StyleSheet,
+   Text,
+   SafeAreaView,
+   Button,
+   FlatList,
+   TextInput,
+   View,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import * as SQLite from "expo-sqlite";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   const [amount, setAmount] = useState("");
+   const [title, setTitle] = useState("");
+   const [shopList, setShopList] = useState([]);
+
+   const db = SQLite.openDatabase("shopListdb.db");
+
+   console.log("====================================");
+   console.log(shopList);
+   console.log("====================================");
+
+   useEffect(() => {
+      db.transaction(
+         (tx) => {
+            tx.executeSql(
+               "create table if not exists shopList (id integer primary key not null, amount int, title text);"
+            );
+         },
+         null,
+         updateList
+      );
+   }, []);
+
+   const updateList = () => {
+      db.transaction(
+         (tx) => {
+            tx.executeSql("select * from shopList;", [], (_, { rows }) =>
+               setShopList(rows._array)
+            );
+         },
+         null,
+         null
+      );
+   };
+
+   const saveItem = () => {
+      db.transaction(
+         (tx) => {
+            tx.executeSql(
+               "insert into shopList (amount, title) values (?, ?);",
+               [parseInt(amount), title]
+            );
+         },
+         null,
+         updateList
+      );
+   };
+
+   const deleteItem = (id) => {
+      db.transaction(
+         (tx) => {
+            tx.executeSql("delete from shopList where id = ?;", [id]);
+         },
+         null,
+         updateList
+      );
+   };
+
+   return (
+      <SafeAreaView style={styles.container}>
+         <TextInput
+            placeholder="Title"
+            onChangeText={(title) => setTitle(title)}
+            value={title}
+         />
+         <TextInput
+            placeholder="Amount"
+            keyboardType="numeric"
+            onChangeText={(amount) => setAmount(amount)}
+            value={amount}
+         />
+         <Button onPress={() => saveItem()} title="Save" />
+         <FlatList
+            style={{ marginLeft: "5%" }}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+               <View style={styles.listcontainer}>
+                  <Text>
+                     {item.title},{item.amount}{" "}
+                  </Text>
+                  <Text
+                     style={{
+                        paddingLeft: 3,
+                        color: "red",
+                     }}
+                     onPress={() => deleteItem(item.id)}
+                  >
+                     done
+                  </Text>
+               </View>
+            )}
+            data={shopList}
+         />
+      </SafeAreaView>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: "white",
+      alignItems: "center",
+      justifyContent: "center",
+      paddingTop: 50,
+   },
+   listcontainer: {
+      padding: 5,
+      display: "flex",
+      flexDirection: "row",
+   },
 });
