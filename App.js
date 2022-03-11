cb2b04f25029bf9daa4f28b4c50ca578c1aeb338
uch2ha <sinyavskiy.d@gmail.com>
2022-03-11 12:00:01 +0200

    done

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+import react, { useState } from "react";
+import * as Contacts from "expo-contacts";
+import * as SMS from "expo-sms";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   const [contacts, setContacts] = useState([]);
+
+   const getContacts = async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === "granted") {
+         const { data } = await Contacts.getContactsAsync({
+            fields: [Contacts.Fields.PhoneNumbers],
+         });
+         if (data.length > 0) {
+            setContacts(data);
+         }
+      }
+   };
+
+   return (
+      <View style={styles.container}>
+         <FlatList
+            data={contacts}
+            renderItem={({ item }) => (
+               <View
+                  style={{
+                     minHeight: 50,
+                     display: "flex",
+                     flexDirection: "row",
+                     justifyContent: "center",
+                  }}
+               >
+                  <Text style={{ paddingRight: 7 }}>{item.name}</Text>
+                  <Text>
+                     {item.phoneNumbers &&
+                        item.phoneNumbers[0] &&
+                        item.phoneNumbers[0].number}
+                  </Text>
+               </View>
+            )}
+            keyExtractor={(item) => item.id}
+         />
+         <Button title="Get Contact" onPress={getContacts} />
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+      paddingTop: 60,
+   },
 });
